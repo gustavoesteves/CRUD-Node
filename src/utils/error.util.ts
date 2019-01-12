9acb7e268c65770d30e1aafc156f8ea0e0f9d3b1
gustavo esteves <gustavo.esteves@gmail.com>
2019-01-12 17:26:41 -0200

    refactory error class

--- src/utils/error.util.ts
+++ src/utils/error.util.ts
@@ -5,17 +5,12 @@ export enum EMassage {
     NotFound = 'NotFound'
 }
 
-export function Errors(status: number, message: string, name?: EMassage): Error {
-    let err = new ResponseError();
-    err.name = name;
-    err.message = message;
-    err.status = status;
-    return err;
-}
-
 export class ResponseError extends Error {
     status: number;
-    constructor() {
+    constructor(status: number, message: string, name?: EMassage) {
         super();
+        this.name = name;
+        this.message = message;
+        this.status = status;
     }
 }
