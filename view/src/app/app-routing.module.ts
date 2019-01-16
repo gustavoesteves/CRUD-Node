effbc037afeba3df861baade2e2ca8f9f9bfa1e3
gustavo esteves <gustavo.esteves@gmail.com>
2019-01-16 12:00:12 -0200

    new design and components

--- view/src/app/app-routing.module.ts
+++ view/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+/**
+ * Import components
+ */
+import { HomeComponent } from "./scenes/layout/home/home.component";
+import { TaskComponent } from "./scenes/task/task/task.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'task', component: TaskComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
