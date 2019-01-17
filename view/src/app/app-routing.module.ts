8061f124962236a813083c3113534fb56f7962c2
gustavo esteves <gustavo.esteves@gmail.com>
2019-01-16 23:07:13 -0200

    task and todo crud design

--- view/src/app/app-routing.module.ts
+++ view/src/app/app-routing.module.ts
@@ -5,10 +5,14 @@ import { Routes, RouterModule } from '@angular/router';
  */
 import { HomeComponent } from "./scenes/layout/home/home.component";
 import { TaskComponent } from "./scenes/task/task/task.component";
+import { TodoCrudComponent } from "./scenes/todo/todo-crud/todo-crud.component";
+import { TaskCrudComponent } from "./scenes/task/task-crud/task-crud.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'task', component: TaskComponent }
+  { path: 'task', component: TaskComponent },
+  { path: 'todo', component: TodoCrudComponent },
+  { path: 'newtask', component: TaskCrudComponent }
 ];
 
 @NgModule({
--- view/src/app/app.module.ts
+++ view/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoComponent } from './scenes/layout/todo/todo.component';
-import { TaskComponent } from './scenes/layout/task/task.component';
+import { TodoComponent } from './scenes/todo/todo/todo.component';
+import { TaskComponent } from './scenes/task/task/task.component';
 import { HomeComponent } from './scenes/layout/home/home.component';
 import { TodoCrudComponent } from './scenes/todo/todo-crud/todo-crud.component';
 import { TaskCrudComponent } from './scenes/task/task-crud/task-crud.component';
